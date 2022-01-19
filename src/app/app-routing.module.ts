12d72e3be0a65d145f85e7f067ac0407053883f2
unknown <chayatron5678@gmail.com>
2022-01-19 23:55:42 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { IndexComponent } from './index/index.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { ServiceComponent } from './service/service.component';
+import { BillComponent } from './bill/bill.component';
+
+ 
+const routes:Routes =[
+    {path:'index', component:IndexComponent},
+    {path:'menu',component:MenuComponent},
+    {path:'order',component:OrderComponent},
+    {path:'service',component:ServiceComponent},
+    {path:'bill',component:BillComponent}
+  ];
+  @NgModule({
+      imports:[RouterModule.forRoot(routes)],
+      exports: [RouterModule]
+  })
+  export class ApRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { ServiceComponent } from './service/service.component';
+import { BillComponent } from './bill/bill.component';
+import { ApRoutingModule } from './app-routing.module';
+ 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MenuComponent,
+    OrderComponent,
+    ServiceComponent,
+    BillComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ApRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
